d9b528c0eab70bd8a4719edd1768907bdeb05299
Rense Lange <renselange@gmail.com>
2022-06-05 23:47:14 +0800

    Update enchantment_pre_process.py

--- enchantment_pre_process.py
+++ enchantment_pre_process.py
@@ -1,24 +1,40 @@
 
 
+# mix in any language, making sure of course that the same
+# "words" end up with the same row index
 
-enchantment_order = {           ####### here alphabetictally ....
-  "Admirado": 0,
-  "Alegre": 1,
-  "Calmo / Tranquilo": 2,
-  "Ciente (percepção ou compreensão aumentada)": 3,
-  "Conectado (unido a algo maior do que você)": 4,
-  "Deslumbrado (impressionado pela beleza de algo)": 5,
-  "Encantado": 6,
-  "Especial (sorte ou privilégio de estar lá)": 7,
-  "Expectante (agradável sensação de ansiedade ou expectativa)": 8,
-  "Importante (grande valorização do momento e seu significado)": 9,
-  "Inspirado": 10,
-  "Introspetivo (atenção concentrado no lugar e tempo)": 11,
-  "Realizado (sentindo-se profundamente satisfeito, gratificado ou completo)": 12,
-  "Rendido (no presença de grandeza ou algo incrível)": 13,
-  "Revigorado": 14,
-  "Surpreendido (sentindo-se perplexo ou surpreso)": 15
+# eu escrevi em português, inglês, francês,
+
+item_to_seq = {           ####### place Portuguese first, and use its order everywhere
+
+  "Admirado": (0,'português'),
+  "Alegre": (1,'português'),
+  "Calmo / Tranquilo": (2,,'português'),
+  "Ciente (percepção ou compreensão aumentada)": (3,'português'),
+  "Conectado (unido a algo maior do que você)": (4,'português'),
+  "Deslumbrado (impressionado pela beleza de algo)": (5,'português'),
+  "Encantado": (6,'português'),
+  "Especial (sorte ou privilégio de estar lá)": (7,'português'),
+  "Expectante (agradável sensação de ansiedade ou expectativa)": (8,'português'),
+  "Importante (grande valorização do momento e seu significado)": (9,'português'),
+  "Inspirado": (10,'português'),
+  "Introspetivo (atenção concentrado no lugar e tempo)": (11,'português'),
+  "Realizado (sentindo-se profundamente satisfeito, gratificado ou completo)": (12,'português'),
+  "Rendido (no presença de grandeza ou algo incrível)": (13,'português'),
+  "Revigorado": (14,'português'),
+  "Surpreendido (sentindo-se perplexo ou surpreso)": (15,'português'),
+
+  "Admirable": (0,'inglês'),
+  "Blij": (1,'holandes')
+
 }
+
+seq_to_item = {}
+
+for w in item_to_seq:
+  seq ,lang= item_to_seq[w]
+  if lang == 'português':    # use portuguese
+    if not seq in seq_to_item: seq_to_item[seq] = w
 
 def pre_process(country_col,enchantment_col):
 

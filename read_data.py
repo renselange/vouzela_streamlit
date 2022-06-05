06d0e2894f5dd163fcaa44b1a5d672dbd814502a
Rense Lange <renselange@gmail.com>
2022-06-05 23:08:39 +0800

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -5,31 +5,13 @@ import datetime
 import streamlit as st
 import openpyxl
 from unidecode import unidecode
+from enchantment_pre_process import enchantment_order
 
 
 ############### re-read only if necessary (via cache)
 ############### make column names into strings
 ############### change enchantment answers from strings to lists (in: "enchantment_iems")
 
-
-enchantment_order = {           ####### here alphabetictally ....
-  "Admirado": 0,
-  "Alegre": 1,
-  "Calmo / Tranquilo": 2,
-  "Ciente (percepção ou compreensão aumentada)": 3,
-  "Conectado (unido a algo maior do que você)": 4,
-  "Deslumbrado (impressionado pela beleza de algo)": 5,
-  "Encantado": 6,
-  "Especial (sorte ou privilégio de estar lá)": 7,
-  "Expectante (agradável sensação de ansiedade ou expectativa)": 8,
-  "Importante (grande valorização do momento e seu significado)": 9,
-  "Inspirado": 10,
-  "Introspetivo (atenção concentrado no lugar e tempo)": 11,
-  "Realizado (sentindo-se profundamente satisfeito, gratificado ou completo)": 12,
-  "Rendido (no presença de grandeza ou algo incrível)": 13,
-  "Revigorado": 14,
-  "Surpreendido (sentindo-se perplexo ou surpreso)": 15
-}
 
 
 @st.cache

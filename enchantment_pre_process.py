ae9a459434ddd3392c2bb0aa8604f74187e5dc9f
Rense Lange <renselange@gmail.com>
2022-06-05 23:08:31 +0800

    Create enchantment_pre_process.py

---
+++ enchantment_pre_process.py
@@ -0,0 +1,25 @@
+
+
+
+enchantment_order = {           ####### here alphabetictally ....
+  "Admirado": 0,
+  "Alegre": 1,
+  "Calmo / Tranquilo": 2,
+  "Ciente (percepção ou compreensão aumentada)": 3,
+  "Conectado (unido a algo maior do que você)": 4,
+  "Deslumbrado (impressionado pela beleza de algo)": 5,
+  "Encantado": 6,
+  "Especial (sorte ou privilégio de estar lá)": 7,
+  "Expectante (agradável sensação de ansiedade ou expectativa)": 8,
+  "Importante (grande valorização do momento e seu significado)": 9,
+  "Inspirado": 10,
+  "Introspetivo (atenção concentrado no lugar e tempo)": 11,
+  "Realizado (sentindo-se profundamente satisfeito, gratificado ou completo)": 12,
+  "Rendido (no presença de grandeza ou algo incrível)": 13,
+  "Revigorado": 14,
+  "Surpreendido (sentindo-se perplexo ou surpreso)": 15
+}
+
+def pre_process(country_col,enchantment_col):
+
+	pass

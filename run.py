f52bceee4745262f617d6b3f44ac47a07a1dad75
Rense Lange <renselange@gmail.com>
2022-06-05 14:24:34 +0800

    Update run.py
    add enchantment

--- run.py
+++ run.py
@@ -24,13 +24,17 @@ page = st.sidebar.radio(
     when, ["1. Inspecione o arquivo de dados", "2. Contagens de frequência simples", "3. Wordclouds de respostas escritas"], index=0
 )
 
+##################### show entire data table ##################
+
 if page.startswith('1.'):
+
 	dd,born = read_vouzela_excel(uploadFile)
 	st.write('Casos classificados mais recentes primeiro')
 	st.dataframe(dd)
 
 	#print(collect_enchantment_responses(dd.columns[20]))
 
+##################### show frequency + figute + stats (if possible) ########
 elif page.startswith('2.'):
 
 	for v in list(dd.columns): 
@@ -43,7 +47,7 @@ elif page.startswith('2.'):
 		except:
 			pass
 
-
+##################### show word clouds for open-ended questions ###########
 elif page.startswith('3.'):
 
 	for f in dd.columns[28:33]:
@@ -52,5 +56,13 @@ elif page.startswith('3.'):
 		st.image(make_image(text))
 		'Por favor, também inspecione as listas de respostas abaixo:'
 		st.write(dd[f].value_counts())
+
+##################### show % of enhcantment items being endorsed #########
+elif page.startswith('4.'):
+
+	# for now, do by sex, but by location would be far better
+	"# enchantment"
+
+
 
 

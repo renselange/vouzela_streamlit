c067db58481017c0cf77fac0ef9498c4cd458015
Rense Lange <renselange@gmail.com>
2022-06-12 15:55:46 +0800

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -44,6 +44,9 @@ def read_vouzela_excel(name,first_day=0,last_day=0):
 
 # from remaining lines, remove any lines with (any) missing values
 # missing can be seen when row index shows a gap ...
+
+	t = t[(t['dateEnd'].dt.date >= first_day) & (t['dateEnd'].dt.date <= last_day)]
+
 	t.dropna(axis=0,inplace=True)
 
 	return t,born
